27429b86f55d187a35960b42724ae97dadff6331
teo67 <teogerst@gmail.com>
2022-06-06 21:49:18 -0700

    animation thingy

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const leaves = document.getElementById("leaves");
 let currentCopied = null;
 const copy = (obj, ignore = false) => {
     if(ignore) {
@@ -11,3 +12,16 @@ const copy = (obj, ignore = false) => {
     obj.classList.add("copied");
     currentCopied = obj;
 }
+
+const spawnLeaf = () => {
+    const newE = document.createElement("div");
+    newE.style.top = `${Math.round(Math.random() * window.innerHeight * 1.5)}px`;
+    newE.style.opacity = `${Math.floor(20 + Math.random() * 30)}%`;
+    newE.style.animationDuration = `${4 + Math.random() * 20}s`;
+    newE.onanimationend = () => {
+        newE.remove();
+    }
+    leaves.appendChild(newE);
+}
+
+setInterval(spawnLeaf, 200);
