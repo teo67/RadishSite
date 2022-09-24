1c985ff140202980189850007defcfafd416e348
teo67 <teogerst@gmail.com>
2022-09-23 23:17:35 -0700

    load more + font fix?

--- gallery/main.js
+++ gallery/main.js
@@ -1,17 +1,23 @@
 const gal = document.getElementById("gal");
+const loadMore = document.getElementById("loadmore");
 
-let srcs = [];
-let ress = [];
+let files = [];
+let next = 0;
 
 const init = async () => {
-    const files = await (await fetch("files.json")).json();
-    for(const file of files) {
-        srcs.push(await (await fetch(`src/${file}.rdsh`)).text());
-        ress.push(`./img/${file}.bmp`);
-    }
+    files = await (await fetch("files.json")).json();
 }
 
-init().then(() => {
+const read = async () => {
+    if(files.length - next <= 5) {
+        loadMore.classList.add("unavailable");
+    }
+    let srcs = [];
+    let ress = [];
+    for(let i = next; i < Math.min(next + 5, files.length); i++) {
+        srcs.push(await (await fetch(`src/${files[i]}.rdsh`)).text());
+        ress.push(`./img/${files[i]}.bmp`);
+    }
     for(let i = 0; i < srcs.length; i++) {
         const newD = document.createElement("section");
         newD.classList.add("gallery");
@@ -28,5 +34,15 @@ init().then(() => {
         newT.appendChild(span); 
         newD.appendChild(newT);
         gal.appendChild(newD);
+    }
+}
+
+init().then(read).then(() => {
+    loadMore.onclick = () => {
+        if(loadMore.classList.contains("unavailable")) {
+            return;
+        }
+        next += 5;
+        read();
     }
 });
