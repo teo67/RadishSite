6f795806cda90dbe07cd9a82e114d68f36f1515c
teo67 <teogerst@gmail.com>
2022-10-11 16:24:36 -0700

    load more + tabs

--- examples/main.js
+++ examples/main.js
@@ -1,23 +1,23 @@
 const texts = document.getElementById("examples");
-// for(const text of texts) {
-//     text.onclick = () => {
-//         copy(text, true);
-//     }
-// }
+const loadMore = document.getElementById("loadmore");
+let files = [];
+let next = 0;
+
 const init = async () => {
-    let returning = [];
     const raw = await fetch("https://api.github.com/repos/teo67/Radish/contents/examples?ref=main");
     const json = await raw.json();
     for(const item of json) {
-        const text = await fetch(item.git_url);
-        const asJson = await text.json();
-        returning.push(atob(asJson.content))
+        files.push(item.git_url);
     }
-    return returning;
 }
 
-init().then(res => {
-    for(const item of res) {
+const read = async() => {
+    if(files.length - next <= 5) {
+        loadMore.classList.add("unavailable");
+    }
+    for(let i = next; i < Math.min(next + 5, files.length); i++) {
+        const asJson = await (await fetch(files[i])).json();
+        const item = atob(asJson.content)
         const lines = item.split("\n");
         const table = document.createElement("table");
         table.onclick = () => {
@@ -26,13 +26,20 @@ init().then(res => {
         table.classList.add("example");
         const tbody = document.createElement("tbody");
         table.appendChild(tbody);
-        for(let i = 0; i < lines.length; i++) {
+        for(let j = 0; j < lines.length; j++) {
             const tr = document.createElement("tr");
             const lineNumber = document.createElement("td");
             lineNumber.classList.add("lineNumber");
-            lineNumber.innerText = `${i + 1}`;
+            lineNumber.innerText = `${j + 1}`;
             const content = document.createElement("td");
-            content.innerText = lines[i];
+            let modLine = lines[j];
+            let numTabs = 0;
+            while(modLine[0] == " ") {
+                modLine = modLine.substring(1);
+                numTabs++;
+            }
+            content.style.paddingLeft = `${numTabs + 2}vw`;
+            content.innerText = modLine;
             content.classList.add("content");
             tr.appendChild(lineNumber);
             tr.appendChild(content);
@@ -40,4 +47,14 @@ init().then(res => {
         }
         texts.appendChild(table);
     }
-})
+}
+
+init().then(read).then(() => {
+    loadMore.onclick = () => {
+        if(loadMore.classList.contains("unavailable")) {
+            return;
+        }
+        next += 5;
+        read();
+    }
+});
